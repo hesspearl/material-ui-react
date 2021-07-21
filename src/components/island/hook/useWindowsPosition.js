2e5b9bdadfc02c88eb15c0b44a3acffca5e7371f
Hessa <hesspearldev@gmail.com>
2021-07-21 21:01:47 +0300

    Update useWindowsPosition.js

--- src/components/island/hook/useWindowsPosition.js
+++ src/components/island/hook/useWindowsPosition.js
@@ -14,7 +14,7 @@ import { useLayoutEffect, useState } from "react";
   useLayoutEffect(() => {
     
     window.addEventListener("scroll", updatePosition);
-  
+  updatePosition();
 
     return () => {
       window.removeEventListener("scroll", updatePosition);
