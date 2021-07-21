e9dadfb1454c06841eca976b8104bdea3640511e
Hessa <hesspearldev@gmail.com>
2021-07-21 19:28:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import Index from "./components/blog/index";
 import Island from "./components/island/app";
-import Tutorial from "./components/tutorial";
+
 
 function App() {
   return (
     <div>
-      {/* <Island/> */}
-      <Index />
+      <Island/> 
+     
     </div>
   );
 }
